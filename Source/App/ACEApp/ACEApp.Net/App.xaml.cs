cf7b28afb7ef37c7284c15f8616d9afb63a5d8d3
Hendrik Jan Schuurkamp <hjs64@live.nl>
2018-04-10 11:16:00 +0200

    Initial: Copy from BitBucket

---
+++ Source/App/ACEApp/ACEApp.Net/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using ACEApp.Net.ViewLayer.Pages;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms;
+
+namespace ACEApp.Net
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new StartPage();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            // Intialisation of Microsoft AppCenter: iOS and Droid
+            AppCenter.Start("ios=cdab3353-65e9-415d-8711-6275c82b5874; android=43f0768f-181e-45a5-a6f7-0f87f8c8e801;", typeof(Analytics), typeof(Crashes));
+        }
+    }
+}
---
+++ Source/App/ACEApp/ACEApp.Net/ViewLayer/Pages/StartPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ACEApp.Net.ViewLayer.Pages
+{
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
