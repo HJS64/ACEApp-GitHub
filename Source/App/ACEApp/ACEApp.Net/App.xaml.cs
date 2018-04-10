dec50ced0ff77c288b3ac13812f720846eddd357
Hendrik Jan Schuurkamp <hjs64@live.nl>
2018-04-10 11:38:09 +0200

    AppSecret for Android (AppCenter)

--- Source/App/ACEApp/ACEApp.Net/App.xaml.cs
+++ Source/App/ACEApp/ACEApp.Net/App.xaml.cs
@@ -21,7 +21,7 @@ namespace ACEApp.Net
             base.OnStart();
 
             // Intialisation of Microsoft AppCenter: iOS and Droid
-            AppCenter.Start("ios=cdab3353-65e9-415d-8711-6275c82b5874; android=43f0768f-181e-45a5-a6f7-0f87f8c8e801;", typeof(Analytics), typeof(Crashes));
+            AppCenter.Start("ios=cdab3353-65e9-415d-8711-6275c82b5874; android=fb82d4fa-27e8-48e5-87e0-217dd9c69b39;", typeof(Analytics), typeof(Crashes));
         }
     }
 }
